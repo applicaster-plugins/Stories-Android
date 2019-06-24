ba2cf8e9dd5075116a0ed5a24a56d04d34574dba
Juan Hernandez <j.hernandez@applicaster.com>
2019-06-24 14:57:28 -0400

    update build.gradle

--- src/main/java/com/mustmobile/stories/StoriesContract.kt
+++ src/main/java/com/mustmobile/stories/StoriesContract.kt
@@ -1,4 +1,17 @@
 package com.mustmobile.stories
 
-class StoriesContract {
+import android.content.Context
+import android.support.v4.app.Fragment
+import com.applicaster.plugin_manager.screen.PluginScreen
+import java.io.Serializable
+import java.util.HashMap
+
+class StoriesContract : PluginScreen {
+    override fun present(context: Context?, screenMap: HashMap<String, Any>?, dataSource: Serializable?, isActivity: Boolean) {
+
+    }
+
+    override fun generateFragment(screenMap: HashMap<String, Any>?, dataSource: Serializable?): Fragment {
+        return Fragment()
+    }
 }
